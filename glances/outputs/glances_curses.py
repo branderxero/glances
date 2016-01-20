5b90bde5fe2f8402519e7f8548ae9f8d88caaeac
andrew harrison <andrew.harrison@brandingbrand.com>
2016-01-20 10:36:17 -0500

    color change

--- glances/outputs/glances_curses.py
+++ glances/outputs/glances_curses.py
@@ -91,11 +91,11 @@ class _GlancesCurses(object):
             else:
                 curses.init_pair(1, curses.COLOR_WHITE, -1)
             curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_RED)
-            curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_GREEN)
+            curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_CYAN)
             curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_BLUE)
             curses.init_pair(5, curses.COLOR_WHITE, curses.COLOR_MAGENTA)
             curses.init_pair(6, curses.COLOR_RED, -1)
-            curses.init_pair(7, curses.COLOR_GREEN, -1)
+            curses.init_pair(7, curses.COLOR_CYAN, -1)
             curses.init_pair(8, curses.COLOR_BLUE, -1)
             try:
                 curses.init_pair(9, curses.COLOR_MAGENTA, -1)
